afb3f6c20917b42f548c5c5aa12e337f315ada28
alexsanchezx <alexsanchez219@yahoo.com>
2014-06-01 23:51:22 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+ 
+        public static void main(String[] args) {
+Dice A = new Dice();
+Dice B = new Dice();
+Dice C = new Dice();
+System.out.println(A.dicearray[0]);
+System.out.println(B.dicearray[1]);
+System.out.println(C.dicearray[2]);
+        }
+}
